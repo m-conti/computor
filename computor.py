b4c0350dcab770b8c888b56f78890715bda6c503
matthieu CONTI <mconti@student.42.fr>
2019-11-04 00:13:29 +0100

    todo : remove list/filter findall n rework findall regex

--- computor.py
+++ computor.py
@@ -1,4 +1,6 @@
 import regex
+import re
+
 
 class Polynome:
 
@@ -10,36 +12,68 @@ class Polynome:
 
 		self.inscribe(pol)
 
+	def __str__(self):
+		return 'a = {a} | b = {b} | c = {c}'.format(a=self.a, b=self.b, c=self.c)
+
 	def inscribe(self, pol):
-		equalities = self.split_equality(pol)
+		self.split_elements(pol)
+		self.parse_elements()
 
-		self.split_elements(equalities[0], False)
-		self.split_elements(equalities[1], True)
+	def split_elements(self, part):
+		self.elements = list(filter(lambda elem: elem[0] or elem[1], re.findall(regex.part, part)))
+		print(self.elements)
 
+	def parse_elements(self):
+		after_equality = False
+		subtract = False
 		for element in self.elements:
-			self.parse_element(element)
+			(value, unknown, power, operand) = element
+			number = float(value) if not subtract else -float(value)
+			self.set_value(number, unknown, power)
+			(subtract, after_equality) = self.set_operand(operand, after_equality)
+
+	def set_value(self, number, unknown, power):
+		if unknown and power == '2':
+			self.a += number
+		elif unknown and (not power or power == '1'):
+			self.b += number
+		elif not unknown or (unknown and power == '0'):
+			self.c += number
 
 	@staticmethod
-	def split_equality(pol):
-		equalities = []
+	def set_operand(operand, after_equality):
+		if operand == '+':
+			return after_equality, after_equality
+		if operand == '-':
+			return not after_equality, after_equality
+		if operand == '=':
+			if after_equality:
+				raise Exception('So many " = "')
+			return True, True
+		if not after_equality:
+			raise Exception('It\'s not an equation')
+		return False, True
 
-		if len(equalities) != 2:
-			raise Exception('" {} " is not an equation.'.format(pol))
-		return equalities
-
-	def split_elements(self, array, after_equal):
-		pass
-
-	def parse_element(self, element):
-		pass
-
-
+# bug {+nb|-nb}
 tests = [
-	'',
+	'1 * x ^ 2 + 2 * x + 3 = 0',
+	'1 x ^ 2 + 2 * x + 3 = 0',
+	'1*x^2 + 2 * x + 3 = 0',
+	'1x^2 + 2 * x + 3 = 0',
+	'1 * x ^ 2 + 2 * x ^ 1 + 3 = 0',
+	'1 * x ^ 2 + 2*x^1 + 3 = 0',
+	'1 * x ^ 2 + 2 * x + 3 * x ^ 0 = 0',
+	'1 * x ^ 2 + 2 * x + 3*x^0 = 0',
+	'1 * x ^ 2 + 2 * x = -3',
+	'1 * x ^ 2 = -2 * x + -3',
+	'0 = -1 * x ^ 2 - 2 * x + -3',
 ]
 
 for test in tests:
-	try :
-		Polynome(test)
+	try:
+		#pol = Polynome(test)
+		#print(pol)
+		for match in re.finditer(regex.part, test):
+			print(match.groupdict())
 	except Exception as error:
-		print(error.message)
+		print('Error : ' + str(error))
--- regex.py
+++ regex.py
@@ -1,3 +1,4 @@
-number = r'([\-+]?(?:[0-9]*[.,]?[0-9]+)|(?:[0-9]+[.,]?[0-9]*))'
-separator_operand = r'[^ \^*\-=+] *([\-+])'
+number = r'(?:[\-+]?(?:[0-9]*[.,]?[0-9]+)|(?:[0-9]+[.,]?[0-9]*))'
+operand = r'(?:[\-+=]|$)'
 
+part = r'(?:(?:(?P<number>{number}) *\*?)? *(?:(?P<unknown>x) *(?:\^ *(?P<power>{number}))?)?) *(?P<operand>{op})'.format(number=number, op=operand)
