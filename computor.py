ac54db8cbda5786d889c105f34589d465f434681
m-conti <mc.heux@gmail.com>
2019-11-04 16:09:56 +0100

    done

--- computor.py
+++ computor.py
@@ -1,79 +1,51 @@
-import regex
-import re
-
-
-class Polynome:
-
-	def __init__(self, pol):
-		self.a = 0
-		self.b = 0
-		self.c = 0
-		self.elements = []
-
-		self.inscribe(pol)
-
-	def __str__(self):
-		return 'a = {a} | b = {b} | c = {c}'.format(a=self.a, b=self.b, c=self.c)
-
-	def inscribe(self, pol):
-		self.split_elements(pol)
-		self.parse_elements()
-
-	def split_elements(self, part):
-		self.elements = list(filter(lambda elem: elem[0] or elem[1], re.findall(regex.part, part)))
-		print(self.elements)
-
-	def parse_elements(self):
-		after_equality = False
-		subtract = False
-		for element in self.elements:
-			(value, unknown, power, operand) = element
-			number = float(value) if not subtract else -float(value)
-			self.set_value(number, unknown, power)
-			(subtract, after_equality) = self.set_operand(operand, after_equality)
-
-	def set_value(self, number, unknown, power):
-		if unknown and power == '2':
-			self.a += number
-		elif unknown and (not power or power == '1'):
-			self.b += number
-		elif not unknown or (unknown and power == '0'):
-			self.c += number
-
-	@staticmethod
-	def set_operand(operand, after_equality):
-		if operand == '+':
-			return after_equality, after_equality
-		if operand == '-':
-			return not after_equality, after_equality
-		if operand == '=':
-			if after_equality:
-				raise Exception('So many " = "')
-			return True, True
-		if not after_equality:
-			raise Exception('It\'s not an equation')
-		return False, True
+from Polynome import Polynome
+import sys
 
 # bug {+nb|-nb}
 tests = [
 	'1 * x ^ 2 + 2 * x + 3 = 0',
-	'1 x ^ 2 + 2 * x + 3 = 0',
-	'1*x^2 + 2 * x + 3 = 0',
-	'1x^2 + 2 * x + 3 = 0',
-	'1 * x ^ 2 + 2 * x ^ 1 + 3 = 0',
-	'1 * x ^ 2 + 2*x^1 + 3 = 0',
-	'1 * x ^ 2 + 2 * x + 3 * x ^ 0 = 0',
-	'1 * x ^ 2 + 2 * x + 3*x^0 = 0',
-	'1 * x ^ 2 + 2 * x = -3',
-	'1 * x ^ 2 = -2 * x + -3',
-	'0 = -1 * x ^ 2 - 2 * x + -3',
+	'1 x ^ 2 + 2 * x + +3 = 0',
+	'1*x^2 + 2 * x - 3 = 0',
+	'1x^2 + 2 * x + -3 = 0',
+	'1 * x ^ 2 + 2 * x ^ 1 - 3 = 0',
+	'1 * x ^ 2 + 2*x^1 + -3 = 0',
+	'1 * x ^ 2 + 2 * x + -3 * x ^ 0 = 0',
+	'1 * x ^ 2 + 2 * x - 3*x^0 = 0',
+	'1 * x ^ 2 + 2 * x = 3',
+	'1 * x ^ 2 = -2 * x + 3',
+	'0 = -1 * x ^ 2 - 2 * x + +3',
+	'x^2 + 2 * x - 3 = 0',
+	'-1 * x ^ 2 + 2 * x + -3 = -2 * x^2',
+	'-x ^ 2 + 2 * x - 3 = -2 * x^2',
+	'1 * x ^ 2 + x + -3 = -x',
+	'2 * x^2 = 0'
 ]
 
-for test in tests:
-	try:
-		#pol = Polynome(test)
-		#print(pol)
-		for match in re.finditer(regex.part, test):
-			print(match.groupdict())
-	except Exception as error:
-		print('Error : ' + str(error))
+
+def print_solution(pol):
+	print('Polynomial degree: ' + str(pol.degree))
+	print(pol.reduced)
+	print(pol.solution())
+
+
+def do_test():
+	for test in tests:
+		try:
+			pol = Polynome(test)
+			print_solution(pol)
+		except Exception as error:
+			print('Error : ' + str(error))
+
+
+if __name__ == '__main__':
+	if len(sys.argv) == 1:
+		print('no argument pass.')
+	if sys.argv[1] == 'tests':
+		do_test()
+	else:
+		for arg in sys.argv[1:]:
+			try:
+				pol = Polynome(arg)
+				print_solution(pol)
+			except Exception as error:
+				print('Error : ' + str(error))
--- regex.py
+++ regex.py
@@ -1,4 +1,4 @@
 number = r'(?:[\-+]?(?:[0-9]*[.,]?[0-9]+)|(?:[0-9]+[.,]?[0-9]*))'
 operand = r'(?:[\-+=]|$)'
 
-part = r'(?:(?:(?P<number>{number}) *\*?)? *(?:(?P<unknown>x) *(?:\^ *(?P<power>{number}))?)?) *(?P<operand>{op})'.format(number=number, op=operand)
+part = r'(?:(?:(?P<number>{number}) *)|(?:(?:(?P<unknown>{number})? *\*? *(?P<negate_unknown>-)?(?P<is_unknown>x)) *(?:\^ *(?P<power>{number}))?)) *(?P<operand>{op})'.format(number=number, op=operand)
