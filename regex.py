f623ebcf8ebbb808ef47ebbef68eb0f3a9d0c43b
matthieu CONTI <mconti@student.42.fr>
2019-11-08 14:22:02 +0100

    regex equation

--- regex.py
+++ regex.py
@@ -2,3 +2,5 @@ number = r'(?:[\-+]?(?:[0-9]*[.,]?[0-9]+)|(?:[0-9]+[.,]?[0-9]*))'
 operand = r'(?:[\-+=]|$)'
 
 part = r'(?:(?:(?P<number>{number}) *\*?)? *(?:(?P<unknown>x) *(?:\^ *(?P<power>{number}))?)?) *(?P<operand>{op})'.format(number=number, op=operand)
+
+equation = r'^(?:{part} ?)+=(?:{part} ?)+'.format(part=part)
